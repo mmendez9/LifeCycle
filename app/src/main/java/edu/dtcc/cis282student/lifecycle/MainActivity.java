f5e1465a3481fa0e1edf0ec4adcb14a2f35c771e
mmendez9 <mmendez9@dtcc.edu>
2017-02-21 12:07:10 -0500

    Toast Added

--- app/src/main/java/edu/dtcc/cis282student/lifecycle/MainActivity.java
+++ app/src/main/java/edu/dtcc/cis282student/lifecycle/MainActivity.java
@@ -3,8 +3,6 @@ package edu.dtcc.cis282student.lifecycle;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -15,62 +13,58 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toast.makeText(getApplicationContext(), "Creating...", Toast.LENGTH_LONG).show();
-
         Log.d(tag,"onCreate()");
-
-        if (savedInstanceState != null) {
-            // Restore the saved data
-            Log.d(tag, "Data restored");
-        }
+        Toast.makeText(getApplicationContext(), "Creating...",
+                Toast.LENGTH_LONG).show();// Toast Message
     }
 
     @Override
     protected void onStart() {
          super.onStart();
-        Toast.makeText(getApplicationContext(), "Starting...", Toast.LENGTH_LONG).show();
-
-        Log.d(tag, "onStart()");
+        Log.d(tag, "onStart()");// Log Message
+        Toast.makeText(getApplicationContext(), "Starting...",
+                Toast.LENGTH_LONG).show();// Toast Message
     }
 
     @Override
     protected  void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(), "Resuming...", Toast.LENGTH_LONG).show();
+        Log.d(tag, "onResume()"); // Log Message
+        Toast.makeText(getApplicationContext(), "Resuming...",
+                Toast.LENGTH_LONG).show();// Toast Message
 
-        Log.d(tag, "onResume()");
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(getApplicationContext(), "Pausing...", Toast.LENGTH_LONG).show();
-
-        Log.d(tag, "onPause()");
+        Log.d(tag, "onPause()");// Log Message
+        Toast.makeText(getApplicationContext(), "Pausing...",
+                Toast.LENGTH_LONG).show();// Toast Message
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(), "Stopping...", Toast.LENGTH_LONG).show();
-        Log.d(tag, "onStop()");
+        Log.d(tag, "onStop()");// Log Message
+        Toast.makeText(getApplicationContext(), "Stopping...",
+                Toast.LENGTH_LONG).show();// Toast Message
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(getApplicationContext(), "Restarting...", Toast.LENGTH_LONG).show();
-
-        Log.d(tag, "onRestart()");
+        Log.d(tag, "onRestart()");// Log Message
+        Toast.makeText(getApplicationContext(), "Restarting...",
+                Toast.LENGTH_LONG).show();// Toast Message
     }
-
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getApplicationContext(), "Destroying...", Toast.LENGTH_LONG).show();
-
-        Log.d(tag, "onDestroy()");
+        Log.d(tag, "onDestroy()");// Log Message
+        Toast.makeText(getApplicationContext(), "Destroying...",
+                Toast.LENGTH_LONG).show();// Toast Message
     }
 }
